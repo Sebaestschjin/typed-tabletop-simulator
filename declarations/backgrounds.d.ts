1b504bb4e75b48da2755685d3cbc20eac6ad28b7
Sebastian Stern <sebastian.stern.42@gmail.com>
2022-04-24 10:00:00 +0200

    feat: Adds backgrounds

---
+++ declarations/backgrounds.d.ts
@@ -0,0 +1,31 @@
+/**
+ * `Backgrounds` is a global which provides the ability to interact with the background.
+ */
+/** @noSelf */
+declare interface Backgrounds {
+    /**
+     * Returns the current background name.
+     */
+    getBackground(): string;
+
+    /**
+     * Returns the image URL of the current custom background, or nil if the current background is not custom.
+     */
+    getCustomURL(): URI;
+
+    /**
+     * Replaces the current background with the background matching the specified `name`.
+     *
+     * @param name The new background name.
+     */
+    setBackground(name: string): boolean;
+
+    /**
+     * Replaces the current background with a custom background loaded from the specified `url`.
+     *
+     * @param url The new custom background.
+     */
+    setCustomURL(url: URI): boolean;
+}
+
+declare const Backgrounds: Backgrounds;
