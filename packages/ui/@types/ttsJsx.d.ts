d6403e8fed86534b978c6a9f7cabac67a7de48a8
Sebastian Stern <sebastian.stern.42@gmail.com>
2023-09-08 12:08:55 +0200

    I guess I don't really know what I'm doing...

--- packages/ui/@types/ttsJsx.d.ts
+++ packages/ui/@types/ttsJsx.d.ts
@@ -1,11 +1,25 @@
 declare namespace JSX {
-    interface IntrinsicElements {
-        button: ButtonElementAttributes;
-        text: TextElementAttributes;
-        horizontalLayout: HorizontalLayoutElementAttributes;
-        horizontalScrollView: HorizontalScrollViewElementAttributes;
-        panel: PanelElementAttributes;
-        verticalLayout: VerticalLayoutElementAttributes;
-        verticalScrollView: VerticalScrollViewElementAttributes;
-    }
+  interface IntrinsicElements {
+    button: ButtonElementAttributes;
+    cell: CellElementAttributes;
+    defaults: {};
+    dropdown: DropdownElementAttributes;
+    gridLayout: GridLayoutElementAttributes;
+    horizontalLayout: HorizontalLayoutElementAttributes;
+    horizontalScrollView: HorizontalScrollViewElementAttributes;
+    image: ImageElementAttributes;
+    inputField: InputFieldElementAttributes;
+    option: OptionElementAttributes;
+    panel: PanelElementAttributes;
+    progressBar: ProgressBarElementAttributes;
+    row: RowElementAttributes;
+    slider: SliderElementAttributes;
+    tableLayout: TableLayoutElementAttributes;
+    text: TextElementAttributes;
+    toggle: ToggleElementAttributes;
+    toggleButton: ToggleButtonElementAttributes;
+    toggleGroup: ToggleGroupElementAttributes;
+    verticalLayout: VerticalLayoutElementAttributes;
+    verticalScrollView: VerticalScrollViewElementAttributes;
+  }
 }
