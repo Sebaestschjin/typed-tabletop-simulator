9c552f2cd6f2ae9114853cc7c82ad0b2d1b2aa46
Sebastian Stern <sebastian.stern.42@gmail.com>
2023-11-28 11:01:25 +0100

    Adds shuffle method

--- packages/lib/src/collection.ts
+++ packages/lib/src/collection.ts
@@ -11,3 +11,12 @@ export const range = (start: number, end?: number): number[] => {
 
   return result;
 };
+
+export const shuffle = (array: any[]) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
