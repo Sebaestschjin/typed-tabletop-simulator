7f9bcfe32a142cde07d93342bb86c16e7346a8fe
Sebastian Stern <sebastian.stern.42@gmail.com>
2023-12-22 21:49:02 +0100

    Fix  json functions

--- packages/declaration/src/moonsharp/json.d.ts
+++ packages/declaration/src/moonsharp/json.d.ts
@@ -2,8 +2,8 @@
 declare interface MoonsharpJson {
   serialize: (this: void, content: any) => string;
   parse: <T>(this: void, content: string) => T;
-  isNull: (value: any) => boolean;
-  null: () => any;
+  isNull: (this: void, value: any) => boolean;
+  null: (this: void) => any;
 }
 
 declare const json: MoonsharpJson;
