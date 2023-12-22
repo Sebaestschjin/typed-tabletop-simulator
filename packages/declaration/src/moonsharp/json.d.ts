a5801d7f42a46da7d0acfed4457f7ac8eda71274
Sebastian Stern <sebastian.stern.42@gmail.com>
2023-12-22 21:42:59 +0100

    Add missing json functions

--- packages/declaration/src/moonsharp/json.d.ts
+++ packages/declaration/src/moonsharp/json.d.ts
@@ -2,6 +2,8 @@
 declare interface MoonsharpJson {
   serialize: (this: void, content: any) => string;
   parse: <T>(this: void, content: string) => T;
+  isNull: (value: any) => boolean;
+  null: () => any;
 }
 
 declare const json: MoonsharpJson;
