ead29767cf2a234f646c9503c0394cb9d6f4a52e
Sebastian Stern <sebastian.stern.42@gmail.com>
2023-12-22 21:51:20 +0100

    F*cking moonsharp

--- packages/declaration/src/moonsharp/json.d.ts
+++ packages/declaration/src/moonsharp/json.d.ts
@@ -2,7 +2,7 @@
 declare interface MoonsharpJson {
   serialize: (this: void, content: any) => string;
   parse: <T>(this: void, content: string) => T;
-  isNull: (this: void, value: any) => boolean;
+  isnull: (this: void, value: any) => boolean;
   null: (this: void) => any;
 }
 
