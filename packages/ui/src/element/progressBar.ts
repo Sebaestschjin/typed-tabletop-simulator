df1cf537b82d726ff59b37aaa81e7e6753fe1892
Sebastian Stern <sebastian.stern.42@gmail.com>
2024-10-28 12:49:35 +0100

    Fixes Progressbar color names

--- packages/ui/src/element/progressBar.ts
+++ packages/ui/src/element/progressBar.ts
@@ -1,14 +1,30 @@
 import { BaseProps, BaseUIElement, ColorLikeProps } from "./base";
+import { convert, Converters } from "./convert";
 
 export interface ProgressBarProps extends BaseProps {
+  /** The percentage of how much is filled in the progress bar. */
   percentage: number;
+  /** The color of the progress bar. */
+  color?: string;
+  /** The color of the empty space of the progress bar. */
   backgroundColor?: string;
-  fillImageColor?: string;
   showPercentageText?: boolean;
 }
 
 export class ProgressBar extends BaseUIElement<ProgressBarProps> {
   constructor(props: ProgressBarProps, children: JSX.Element[]) {
-    super("progressBar", { ...props, showPercentageText: props.showPercentageText ?? false }, { children: children });
+    super(
+      "progressBar",
+      {
+        ...props,
+        showPercentageText: props.showPercentageText ?? false,
+      },
+      { children, converters }
+    );
   }
 }
+
+const converters: Converters = {
+  color: convert.rename("fillImageColor"),
+  backgroundColor: convert.rename("color"),
+};
