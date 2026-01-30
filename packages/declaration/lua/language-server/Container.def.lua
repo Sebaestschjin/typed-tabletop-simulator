8656e500b8c2267d9e3b9dbf2f245184b620b368
SoundOfScooting <soundofsealing@gmail.com>
2026-01-30 15:30:23 -0500

    Improve behavior docs

--- packages/declaration/lua/language-server/Container.def.lua
+++ packages/declaration/lua/language-server/Container.def.lua
@@ -1,8 +1,22 @@
 ---@meta
 
+---
+--- The Container behavior is present on Container objects such as Bags, Stacks and Decks.
+---
 ---@class tts__ContainerBehaviour
 local Container
 
---- Opens the search window of the container for the given player.
----@param playerColor tts__PlayerColor
-function Container.Search(playerColor) end
+---
+---Show the Search window for the container to `player`. If you specify `max_cards` then the search will be limited to that many cards from the top of the deck.
+---
+---@param playerColor tts__PlayerColor The color of the player to show the Search window to.
+---@param max_cards? integer Optional maximum number of cards to show.
+---
+---**Example**
+---```
+---deck.Container.search(Player.Blue, 3)
+---```
+--- <br>
+function Container.search(playerColor, max_cards) end
+---@deprecated Use "search" instead
+Container.Search = Container.search
