a7554c21e4a175772f6fd2a5703f342ce0226b6b
Sebastian Stern <sebastian.stern.42@gmail.com>
2026-03-15 15:58:15 +0100

    Expose combineDecks method

--- packages/forge/src/index.ts
+++ packages/forge/src/index.ts
@@ -1,6 +1,6 @@
 import { createAssetBundle } from "./object/assetBundle.js";
 import { createBag } from "./object/bag.js";
-import { createDeck } from "./object/deck.js";
+import { createDeck, combineDecks } from "./object/deck.js";
 import { createModel } from "./object/model.js";
 import { createSaveFile } from "./object/saveFile.js";
 import { createTile } from "./object/tile.js";
@@ -12,6 +12,7 @@ export default {
   createAssetBundle,
   createBag,
   createDeck,
+  combineDecks,
   createModel,
   createStates,
   createTile,
